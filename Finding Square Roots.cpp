5015dbf0e0e1e9afa53003e028f47834ca28901f
NEELAM SINGH <89604942+NEELAMSINGHH@users.noreply.github.com>
2021-11-01 21:17:00 +0530

    Create Finding Square Roots.cpp

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int T,N;
+    cin>>T;
+  for(int i=0;i<T;i++){
+       cin>>N;
+       int t=sqrt(N);
+       cout<<round(t)<<endl;
+      }
+    return 0;
+    
+   }
