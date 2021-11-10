98a3022112abe239a401572dfce6974c347d53ea
NEELAM SINGH <89604942+NEELAMSINGHH@users.noreply.github.com>
2021-11-10 18:52:26 +0530

    Create Helping Chef.cpp

---
+++ Helping Chef.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    while(n--){
+        int x;
+        cin>>x;
+        if(x<10){
+            cout<<"Thanks for helping Chef!"<<endl;
+        }
+        else{
+            cout<<"-1"<<endl;
+        }
+     }
+     return 0;
+}
